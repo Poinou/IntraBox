87500a4d606f4d79c7e0eab6112358fdef0118fe
Tonio Poinou <el.poinou@gmail.com>
2012-01-24 16:28:22 +0100

    Nouvelle modif (24/01/12)

--- lib/IntraBox.pm
+++ lib/IntraBox.pm
@@ -1,12 +1,13 @@
 package IntraBox;
 use Dancer ':syntax';
 use Digest::SHA1;
+use strict;
+use warnings;
 
 our $VERSION = '0.1';
 
-
 #Récupération du nom
-my $user    = $ENV{'REMOTE_USER'};
+my $user = $ENV{'REMOTE_USER'};
 
 #Vérification si il est admin
 #Récupération du groupe dans lequel il est
@@ -15,13 +16,15 @@ my $isAdmin;
 my $user_group;
 my $user_size_file_limit;
 my $user_size_space_limit;
-($isAdmin, $user_group, $user_size_file_limit, $user_size_space_limit) = recuperation_donnees_session_user($user);
+( $isAdmin, $user_group, $user_size_file_limit, $user_size_space_limit ) =
+  recuperation_donnees_session_user($user);
 
 #Récupération de la taille actuelle utilisée de son espace personnel
 my $user_space_used;
 $user_space_used = calcul_used_space($user);
 
-
+#Calcul de l'espace libre de user
+my $user_space_free = $user_size_space_limit - $user_space_used;
 
 
 #--------- ROUTEES -------
@@ -31,10 +34,10 @@ get '/' => sub {
 "Vous pouvez uploader vos fichiers en renseignant tous les champs nécessaires";
 	template 'index',
 	  {
-		info_color => $info_color,
-		message    => $message,
-		user_space_used => ($user_space_used/(1024*1024)),
-		user_size_space_limit => ($user_size_space_limit/(1024*1024))
+		info_color            => $info_color,
+		message               => $message,
+		user_space_used       => ( $user_space_used / ( 1024 * 1024 ) ),
+		user_size_space_limit => ( $user_size_space_limit / ( 1024 * 1024 ) )
 	  };
 };
 
@@ -45,8 +48,8 @@ get '/admin' => sub {
 post '/upload' => sub {
 	upload_file();
 };
-#--------- /ROUTEES -------
 
+#--------- /ROUTEES -------
 
 #----------Sub Routines --------
 
@@ -71,8 +74,6 @@ sub upload_file {
 		my @name_files;
 		my @hash_names;
 		my $total_size;
-
-		
 
 		my $controle_valid = 1;
 
@@ -144,6 +145,8 @@ sub upload_file {
 				$info_color = "info-vert";
 
 				$message = "Upload terminé des fichiers : $temp_message";
+				
+				
 			}
 		}
 	}
@@ -172,9 +175,9 @@ sub count_files {
 }
 
 sub randomposition {
-   my $chaine = $_[0];
-   return int rand length $chaine;
-   } 
+	my $chaine = $_[0];
+	return int rand length $chaine;
+}
 
 sub generate_aleatoire_key {
 	my $lenght = $_[0];
@@ -183,8 +186,8 @@ sub generate_aleatoire_key {
 	my $list_char =
 	  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 	for ( $i = 1 ; $i < $lenght ; ++$i ) {
-		 my $temp_key = substr( $list_char, randomposition($list_char), 1 );
-		 $key = "$key$temp_key";
+		my $temp_key = substr( $list_char, randomposition($list_char), 1 );
+		$key = "$key$temp_key";
 	}
 	return $key;
 }
@@ -194,23 +197,30 @@ sub generate_aleatoire_key {
 #	my $size_file = -s "/$chemin_fic";
 #	return $size_file;
 #}
-#--- /UPLOAD ----
 
+#--- /UPLOAD ----
 
 #--- Infos User ---
 
 sub recuperation_donnees_session_user {
 	my $user = $_[0];
-	my $isAdmin = false;
-	my $user_group = "eleves";
-	my $user_size_file_limit = 100*1024*1024;
-	my $user_size_space_limit = 400*1024*1024;
-	return ($isAdmin, $user_group, $user_size_file_limit, $user_size_space_limit);
+	my $isAdmin;
+	if ($user eq "abourgan") {
+		$isAdmin = true;
+
+	}
+	else { $isAdmin = false; }
+
+	my $user_group            = "eleves";
+	my $user_size_file_limit  = 100 * 1024 * 1024;
+	my $user_size_space_limit = 400 * 1024 * 1024;
+	return ( $isAdmin, $user_group, $user_size_file_limit,
+		$user_size_space_limit );
 }
 
 sub calcul_used_space {
-	my $user = $_[0];
-	my $user_space_used = 10*1024*1024;
+	my $user            = $_[0];
+	my $user_space_used = 10 * 1024 * 1024;
 	return $user_space_used;
 }
 
