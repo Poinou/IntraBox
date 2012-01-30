333558696cd3f2475fdd680aea0ec6a562aedb7d
Julien Girault <julien.emac@gmail.com>
2012-01-30 12:08:41 +0100

    New SQL file according to the new database

--- SQL/data_model.sql
+++ SQL/data_model.sql
@@ -6,78 +6,83 @@ CREATE SCHEMA IF NOT EXISTS `intrabox` DEFAULT CHARACTER SET utf8 ;
 USE `intrabox` ;
 
 -- -----------------------------------------------------
--- Table `intrabox`.`usergroup`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `intrabox`.`usergroup` (
-  `id_usergroup` INT NOT NULL ,
-  `rule_type` VARCHAR(45) NOT NULL ,
-  `rule` VARCHAR(45) NOT NULL ,
-  `quota` TINYINT NOT NULL ,
-  `size_max` TINYINT NOT NULL ,
-  `expiration_max` TINYINT NOT NULL ,
-  PRIMARY KEY (`id_usergroup`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `intrabox`.`admin`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `intrabox`.`admin` (
-  `id_admin` INT NOT NULL ,
-  `id_user` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id_admin`) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `intrabox`.`status`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `intrabox`.`status` ;
+
 CREATE  TABLE IF NOT EXISTS `intrabox`.`status` (
-  `id_status` INT NOT NULL ,
+  `id_status` INT(11) NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id_status`) )
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `intrabox`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `intrabox`.`user` ;
+
+CREATE  TABLE IF NOT EXISTS `intrabox`.`user` (
+  `id_user` INT(11) NOT NULL ,
+  `login` VARCHAR(45) NOT NULL ,
+  `admin` TINYINT(1) NOT NULL DEFAULT false ,
+  PRIMARY KEY (`id_user`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `intrabox`.`deposit`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `intrabox`.`deposit` ;
+
 CREATE  TABLE IF NOT EXISTS `intrabox`.`deposit` (
-  `id_deposit` INT NOT NULL ,
+  `id_deposit` INT(11) NOT NULL ,
   `id_user` VARCHAR(30) NOT NULL ,
   `download_code` VARCHAR(45) NOT NULL ,
   `area_access_code` VARCHAR(45) NULL DEFAULT NULL ,
   `area_to_email` VARCHAR(45) NULL DEFAULT NULL ,
-  `area_size` TINYINT NULL DEFAULT NULL ,
-  `opt_acknowledgement` TINYINT(1) NOT NULL DEFAULT FALSE ,
-  `opt_downloads_report` TINYINT(1) NOT NULL DEFAULT FALSE ,
+  `area_size` TINYINT(4) NULL DEFAULT NULL ,
+  `opt_acknowledgement` TINYINT(1) NOT NULL DEFAULT '0' ,
+  `opt_downloads_report` TINYINT(1) NOT NULL DEFAULT '0' ,
   `opt_comment` TINYTEXT NULL DEFAULT NULL ,
   `opt_password` VARCHAR(20) NULL DEFAULT NULL ,
-  `id_status` INT NOT NULL ,
-  `expiration_days` TINYINT NOT NULL ,
+  `id_status` INT(11) NOT NULL ,
+  `expiration_days` TINYINT(4) NOT NULL ,
   `expiration_date` DATE NOT NULL ,
   `created_date` DATE NOT NULL ,
   `created_ip` VARCHAR(19) NOT NULL ,
   `created_useragent` VARCHAR(150) NOT NULL ,
-  PRIMARY KEY (`id_deposit`, `id_status`) ,
+  `user_id_user` INT(11) NOT NULL ,
+  PRIMARY KEY (`id_deposit`, `id_status`, `user_id_user`) ,
   INDEX `fk_deposits_status1` (`id_status` ASC) ,
+  INDEX `fk_deposit_user1` (`user_id_user` ASC) ,
   CONSTRAINT `fk_deposits_status1`
     FOREIGN KEY (`id_status` )
     REFERENCES `intrabox`.`status` (`id_status` )
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_deposit_user1`
+    FOREIGN KEY (`user_id_user` )
+    REFERENCES `intrabox`.`user` (`id_user` )
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `intrabox`.`file`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `intrabox`.`file` ;
+
 CREATE  TABLE IF NOT EXISTS `intrabox`.`file` (
-  `id_file` INT NOT NULL ,
-  `id_deposit` INT NOT NULL ,
+  `id_file` INT(11) NOT NULL ,
+  `id_deposit` INT(11) NOT NULL ,
   `name` VARCHAR(60) NOT NULL ,
   `size` FLOAT NOT NULL ,
-  `on_server` TINYINT(1) NOT NULL DEFAULT TRUE ,
+  `on_server` TINYINT(1) NOT NULL DEFAULT '1' ,
   PRIMARY KEY (`id_file`, `id_deposit`) ,
   INDEX `fk_files_deposits` (`id_deposit` ASC) ,
   CONSTRAINT `fk_files_deposits`
@@ -85,21 +90,24 @@ CREATE  TABLE IF NOT EXISTS `intrabox`.`file` (
     REFERENCES `intrabox`.`deposit` (`id_deposit` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `intrabox`.`download`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `intrabox`.`download` ;
+
 CREATE  TABLE IF NOT EXISTS `intrabox`.`download` (
-  `id_download` INT NOT NULL ,
-  `id_deposit` INT NOT NULL ,
-  `id_file` INT NOT NULL ,
+  `id_download` INT(11) NOT NULL ,
+  `id_deposit` INT(11) NOT NULL ,
+  `id_file` INT(11) NOT NULL ,
   `ip` VARCHAR(19) NOT NULL ,
   `useragent` VARCHAR(150) NOT NULL ,
   `start_date` DATE NOT NULL ,
   `end_date` DATE NULL DEFAULT NULL ,
-  `finished` TINYINT(1) NOT NULL DEFAULT FALSE ,
+  `finished` TINYINT(1) NOT NULL DEFAULT '0' ,
   PRIMARY KEY (`id_download`, `id_deposit`, `id_file`) ,
   INDEX `fk_downloads_files1` (`id_file` ASC, `id_deposit` ASC) ,
   CONSTRAINT `fk_downloads_files1`
@@ -107,16 +115,26 @@ CREATE  TABLE IF NOT EXISTS `intrabox`.`download` (
     REFERENCES `intrabox`.`file` (`id_file` , `id_deposit` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 -- -----------------------------------------------------
--- Table `intrabox`.`user`
+-- Table `intrabox`.`usergroup`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `intrabox`.`user` (
-  `id_user` int(11) NOT NULL,
-  `login` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `intrabox`.`usergroup` ;
+
+CREATE  TABLE IF NOT EXISTS `intrabox`.`usergroup` (
+  `id_usergroup` INT(11) NOT NULL ,
+  `rule_type` VARCHAR(45) NOT NULL ,
+  `rule` VARCHAR(45) NOT NULL ,
+  `quota` TINYINT(4) NOT NULL ,
+  `size_max` TINYINT(4) NOT NULL ,
+  `expiration_max` TINYINT(4) NOT NULL ,
+  PRIMARY KEY (`id_usergroup`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
